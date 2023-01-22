2ea98f3813661a5023a4c3959fb16bff746be64e
Mark Renan <markrenan.21@gmail.com>
2023-01-22 16:36:21 +0800

    fix: Database php conflict

--- db_connect.php
+++ db_connect.php
@@ -2,15 +2,5 @@
 
 // Connects database using XAMPP credentials 
 $dbconn = mysqli_connect('localhost', 'root', '', 'adblab_libmgmtsystem');
-<<<<<<< HEAD
-?>
-=======
-
-$read_departments = mysqli_query($dbconn, "SELECT * FROM read_departments");
-
-$read_colleges = mysqli_query($dbconn, "SELECT * FROM read_colleges");
-
-$read_programs = mysqli_query($dbconn, "SELECT * FROM read_programs");
 
 ?>
->>>>>>> 42649393f3143785897d123123f8bbd7c463c5a5
--- db_read.php
+++ db_read.php
@@ -2,8 +2,15 @@
 
 // Read Departments
 $read_departments = mysqli_query($dbconn, "SELECT * FROM read_departments");
+
+// Read Authors
 $read_authors = mysqli_query($dbconn, "SELECT * FROM read_authors");
 
+// Read Colleges
+$read_colleges = mysqli_query($dbconn, "SELECT * FROM read_colleges");
+
+// Read Programs
+$read_programs = mysqli_query($dbconn, "SELECT * FROM read_programs");
 
 
 ?>
