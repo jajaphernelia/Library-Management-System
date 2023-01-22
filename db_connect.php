c14c6f151066964ab2b5649e4a25d6ae016b8a76
Mark Renan <markrenan.21@gmail.com>
2023-01-23 05:46:55 +0800

    refactor: Database php file

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,15 @@
 <?php
 
+$db_server = 'localhost';
+$db_username = 'root';
+$db_password = '';
+$db_name = 'adblab_libmgmtsystem';
+
 // Connects database using XAMPP credentials 
-$dbconn = mysqli_connect('localhost', 'root', '', 'adblab_libmgmtsystem');
+$dbconn = mysqli_connect($db_server, $db_username, $db_password, $db_name);
 
-
-
+if ($dbconn->connect_error) {
+    die("Connection failed: " . $dbconn->connect_error);
+}
 
 ?>
