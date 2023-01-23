3ff2364c897203d775ace2f9e3e5a56fbd2275da
Mark <78240560+xyzuser12@users.noreply.github.com>
2023-01-23 23:45:11 +0800

    catalog type change tab name

--- catalog_type.php
+++ catalog_type.php
@@ -13,7 +13,7 @@ include 'db_delete.php';
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Author | libPLM</title>
+  <title>Catalog Type | libPLM</title>
 
   <link rel="stylesheet" href="assets/css/main/app.css" />
   <link rel="stylesheet" href="assets/css/main/app-dark.css" />
